90ede34690120521fe7f86bf9a2c9886774c6758
emanzelekha <emanzelekha@gmail.com>
2016-12-17 22:52:18 -0800

    commitEmy4

--- app/src/main/java/com/example/ahmed/mosas_hotel/MainActivity.java
+++ app/src/main/java/com/example/ahmed/mosas_hotel/MainActivity.java
@@ -1,13 +1,95 @@
 package com.example.ahmed.mosas_hotel;
 
+import android.app.ProgressDialog;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.graphics.PixelFormat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.view.Window;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.Toast;
+
+import com.example.ahmed.mosas_hotel.Home.home_activity;
+import com.loopj.android.http.JsonHttpResponseHandler;
+import com.loopj.android.http.RequestParams;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import cz.msebera.android.httpclient.Header;
 
 public class MainActivity extends AppCompatActivity {
 
+    int finish=0;
+    public void onAttachedToWindow() {
+        super.onAttachedToWindow();
+        Window window = getWindow();
+        window.setFormat(PixelFormat.RGBA_8888);
+    }
+    /** Called when the activity is first created. */
+    Thread splashTread;
+    String[] MainDep, MainDepId, SubDep, SubDepId;
+    String MainDep1 = "", MainDepId1 = "", SubDep1 = "", SubDepId1 = "",SubDep1e = "", SubDepId1e = "";
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        StartAnimations();
     }
+    private void StartAnimations() {
+        Animation anim = AnimationUtils.loadAnimation(this, R.anim.alph);
+        anim.reset();
+        LinearLayout l=(LinearLayout) findViewById(R.id.lin_lay);
+        l.clearAnimation();
+        l.startAnimation(anim);
+
+        anim = AnimationUtils.loadAnimation(this, R.anim.translate);
+        anim.reset();
+        ImageView iv = (ImageView) findViewById(R.id.splash);
+        View text =  findViewById(R.id.splash_text);
+
+        iv.clearAnimation();
+        iv.startAnimation(anim);
+        text.clearAnimation();
+        text.startAnimation(anim);
+        splashTread = new Thread() {
+            @Override
+            public void run() {
+                try {
+                    int waited = 0;
+                    // Splash screen pause time
+                    while (waited < 3500) {
+                        sleep(100);
+                        waited += 100;
+                    }
+                    Intent intent = new Intent(MainActivity.this,
+                            home_activity.class);
+                    intent.setFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION);
+
+                    startActivity(intent);
+                    MainActivity.this.finish();
+
+
+
+                } catch (InterruptedException e) {
+                    // do nothing
+                } finally {
+                    MainActivity.this.finish();
+                }
+
+            }
+        };
+        splashTread.start();
+
+    }
+
 }
+
